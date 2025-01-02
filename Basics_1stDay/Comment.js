3a6b32583018df4f575ccf6386a4b081a359d0e6
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-01-02 21:50:01 +0545

    Comments in JS

---
+++ Basics_1stDay/Comment.js
@@ -0,0 +1,15 @@
+// This is a simple program demonstrating Comments and in JavaScript
+//Comments in JavaScript can be written in two ways:
+// 1. Single-line comments using `//`
+// 2. Multi-line comments using `/*` and `*/`
+
+
+// Single-line comment
+let x ="Nitesh Joshi";
+console.log(x);
+
+// Multi-line comment
+/*let y = "Hello World";
+console.log(y);*/
+
+// This is a simple program demonstrating Comments and in JavaScript
